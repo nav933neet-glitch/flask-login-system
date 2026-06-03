39cc9876a0fadde5096ef4b215c059b177ecb170
nav933neet-glitch <nav933neet@gmail.com>
2026-06-03 17:37:46 +0530

    sqlite3 features

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
-
+import sqlite3
 
 app = Flask(__name__)
+
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    email TEXT,
+    password TEXT
+)
+""")
+
+conn.commit()
+conn.close()
 
 users = {}
 
@@ -21,10 +36,16 @@ def register_user():
     email = request.form['email']
     password = request.form['password']
     
-    users[username] = {
-    "email": email,
-    "password": password
-}
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+     (username, email, password)
+    )
+
+    conn.commit()
+    conn.close()
     
     return f"Mr/Miss {username} your registered successfully ✅"
 
@@ -34,7 +55,18 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    if username in users and users[username]["password"] == password:
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+    "SELECT * FROM users WHERE username=? AND password=?",
+    (username, password)
+    )
+
+    user = cursor.fetchone()
+    conn.close()
+    
+    if user:
         return "Login Successful ✅"
     else:
         return "Invalid Username or Password ❌"
